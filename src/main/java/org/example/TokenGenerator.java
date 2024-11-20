39d60e7135761ce783f9fed76571565b97030d9d
bieniekl <bieniekl@icloud.com>
2024-11-20 19:08:02 +0100

    resolving issues with commit

--- src/main/java/org/example/TokenGenerator.java
+++ src/main/java/org/example/TokenGenerator.java
@@ -1,14 +1,16 @@
 package org.example;
+
 import com.mifmif.common.regex.Generex;
 
 
 public class TokenGenerator {
-    private static String regex5 = "([a-z]|[A-Z]|[0-9]|[!@#$%^&*()]){5}";
-    private static String regex10 = "([a-z]|[A-Z]|[0-9]|[!@#$%^&*()]){10}";
-    private static String regex15 = "([a-z]|[A-Z]|[0-9]|[!@#$%^&*()]){15}";
+    private static final String regex5 = "([a-z]|[A-Z]|[0-9]|[!@#$%^&*()]){5}";
+    private static final String regex10 = "([a-z]|[A-Z]|[0-9]|[!@#$%^&*()]){10}";
+    private static final String regex15 = "([a-z]|[A-Z]|[0-9]|[!@#$%^&*()]){15}";
     //   public static String token;
     private static Generex generex;
     private static String token;
+
     public static String tokenGenerator(int characters) {
 
         if (characters != 5 && characters != 10 && characters != 15) {
@@ -16,22 +18,22 @@ public class TokenGenerator {
         }
 
 
-switch (characters) {
-    case 5:
-        generex = new Generex(regex5);
-        // Generate random String
-        token = generex.random();
-        break;
-    case 10:
-        generex = new Generex(regex10);
-        token = generex.random();
-        break;
-    case 15:
-        generex = new Generex(regex15);
-        token = generex.random();
-        break;
-}
-                return token;
-            }
+        switch (characters) {
+            case 5:
+                generex = new Generex(regex5);
+                // Generate random String
+                token = generex.random();
+                break;
+            case 10:
+                generex = new Generex(regex10);
+                token = generex.random();
+                break;
+            case 15:
+                generex = new Generex(regex15);
+                token = generex.random();
+                break;
         }
+        return token;
+    }
+}
 
