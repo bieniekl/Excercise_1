0244fba2cb553da5a6e505a7a7bb1eac7bd69f7f
bieniekl <bieniekl@icloud.com>
2024-11-21 08:35:19 +0100

    cleanup

--- src/main/java/org/example/TokenGenerator.java
+++ src/main/java/org/example/TokenGenerator.java
@@ -6,20 +6,17 @@ import com.mifmif.common.regex.Generex;
 public class TokenGenerator {
     private static final String regex = "([a-z]|[A-Z]|[0-9]|[!@#$%^&*()])";
     private static final String regex2 = "[a-zA-Z0-9!@#$%^&*()]";
-    private static Generex generex;
-    private static String token;
-    private static String param;
 
     public static String tokenGenerator(int characters) {
 
         if (characters != 5 && characters != 10 && characters != 15) {
             throw new IllegalArgumentException("Invalid token length! Supported lengths are 5, 10, and 15.");
         }
-            param = String.valueOf(characters);
-            String regexFinal = (regex + "{" + param + "}");
-            generex = new Generex(regexFinal);
+        String param = String.valueOf(characters);
+        String regexFinal = (regex + "{" + param + "}");
+        Generex generex = new Generex(regexFinal);
             // Generate random String
-            token = generex.random();
+        String token = generex.random();
 
         return token;
     }
